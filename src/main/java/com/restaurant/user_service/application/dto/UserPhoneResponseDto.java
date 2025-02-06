8e1bbf659edce8b2cb0962844f65ec8b4bda6825
ortizjulian <julian.ortixs@gmail.com>
2025-02-06 13:25:44 -0500

    fix UserPhoneDto

--- src/main/java/com/restaurant/user_service/application/dto/UserPhoneResponseDto.java
+++ src/main/java/com/restaurant/user_service/application/dto/UserPhoneResponseDto.java
@@ -1,9 +1,17 @@
 package com.restaurant.user_service.application.dto;
 
 public class UserPhoneResponseDto {
-    public String phone;
+    private String phone;
 
     public UserPhoneResponseDto(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 }
